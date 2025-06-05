7714ababb5aec42394807e6e7019c70cfe64cdf6
Kyle Ferguson <kyleferguson91@gmail.com>
2025-06-05 17:22:32 -0400

    created attack function for ships and a missed list

--- Gameboard.js
+++ Gameboard.js
@@ -4,11 +4,11 @@ class Gameboard {
     constructor() {
 
         this.board = this.createBoard()
-        
+        this.missedAttacks = []
 
     }
 
-       createBoard() {
+    createBoard() {
         //return a 2d array..
         //make a new board
         let board = [];
@@ -31,12 +31,12 @@ class Gameboard {
 
         return board;
 
-    } 
+    }
 
     placeShip(ship, direction, rowstart, colstart) {
         //what is involved in placing a ship?
         //we can read its length
-       let length = ship.length;
+        let length = ship.length;
         //we are either placing it horizontally or vertically..
         //lets call a direction check
         //other params above
@@ -49,26 +49,25 @@ class Gameboard {
 
         //coordinates are correct, lets iterate over them
 
-        for (let i = 0; i<ShipCoords.length; i++)
-        {
-      
-                // we are now iterating over each individual array
-                //we have a [row,column format..]
-                let row = ShipCoords[i][0]
-                let column = ShipCoords[i][1]
-                this.board[row][column] = 'ship'
-            
+        for (let i = 0; i < ShipCoords.length; i++) {
+
+            // we are now iterating over each individual array
+            //we have a [row,column format..]
+            let row = ShipCoords[i][0]
+            let column = ShipCoords[i][1]
+            this.board[row][column] = ship
+
         }
 
-         return this.board;
-        
+        return this.board;
+
     }
 
-    getCoordinateList(direction, rowstart, colstart,length) {
+    getCoordinateList(direction, rowstart, colstart, length) {
         //in charge of producing horizontal/vertial ship
         //this should output the coordinates we need
-       
-        
+
+
         let coordinateList = [];
         //either we are going horizontal, or vertical
         //horizontal:
@@ -79,8 +78,7 @@ class Gameboard {
 
         //we need only check if the coordinates are valid once
         //once we do, we will continue in only one path that has been checked for success
-        if (!this.checkValidCoordinate(direction, rowstart,colstart,length))
-        {
+        if (!this.checkValidCoordinate(direction, rowstart, colstart, length)) {
             return false;
 
         }
@@ -88,83 +86,104 @@ class Gameboard {
         //we use another function to check for valid placement once..
         if (direction == 'horizontal') {
             //return the coordinates, do not set them!
-           
+
 
             let counter = 0;
-            while (counter<length)
-            {
-                 coordinateList.push([rowstart, colstart+counter])
+            while (counter < length) {
+                coordinateList.push([rowstart, colstart + counter])
                 counter++
             }
-            
+
         }
         else {
             //vertical
             //row+1 col <9>0
 
             let counter = 0;
-            while (counter<length)
-            {
-                 coordinateList.push([rowstart+counter, colstart])
+            while (counter < length) {
+                coordinateList.push([rowstart + counter, colstart])
                 counter++
             }
 
         }
 
-return coordinateList
+        return coordinateList
     }
 
 
-    checkValidCoordinate(direction, rowstart, colstart, length)
-    {
+    checkValidCoordinate(direction, rowstart, colstart, length) {
         //we are either checking vertial or horizontal
-        if (direction == 'horizontal')
-        {
-            
+        if (direction == 'horizontal') {
+
 
             //we must check the x axis in an increasing manner
-            return colstart + length <= 10 && this.checkCollision(direction,rowstart,colstart,length)
+            return colstart + length <= 10 && this.checkCollision(direction, rowstart, colstart, length)
         }
-        else 
-        {
+        else {
             //check the y
-            return rowstart+length <= 10 && this.checkCollision(direction,rowstart,colstart,length)
+            return rowstart + length <= 10 && this.checkCollision(direction, rowstart, colstart, length)
 
         }
 
 
     }
 
-        checkCollision(direction, rowstart, colstart, length)
-        {
-            if (direction == 'horizontal')
-            {
-                //ensure every cell in colstart+lenth is not occupied for horizontal ships
-                for (let i = 0; i<length; i++)
-                {
-                    if (this.board[rowstart][colstart+i] == 'ship')
-                    {
-                        return false;
-                    }
+    checkCollision(direction, rowstart, colstart, length) {
+        if (direction == 'horizontal') {
+            //ensure every cell in colstart+lenth is not occupied for horizontal ships
+            for (let i = 0; i < length; i++) {
+                if (this.board[rowstart][colstart + i] == 'ship') {
+                    return false;
                 }
-                return true
             }
-            else
-            {
-                              //ensure every cell in row+lenth is not occupied for vertical ships
-                for (let i = 0; i<length; i++)
-                {
-                    if (this.board[rowstart+i][colstart] == 'ship')
-                    {
-                        return false;
-                    }
+            return true
+        }
+        else {
+            //ensure every cell in row+lenth is not occupied for vertical ships
+            for (let i = 0; i < length; i++) {
+                if (this.board[rowstart + i][colstart] == 'ship') {
+                    return false;
                 }
-                return true
             }
-           
+            return true
         }
 
+    }
 
+
+
+
+
+    attack(row, col)
+    {
+                if (this.board[row][col] == 'shiphit')
+        {
+                //do nothing because the ship has already been hit..
+                //we could call a dom update here to affect the square 
+                //or say already clicked
+        }
+        if (this.board[row][col] == ship)
+        {
+            //call ship.hit
+            ship.hit();
+
+            //should we handle hit with the dom?
+
+
+            //and then set the field to already hit
+            this.board[row][col] = 'shiphit';
+        }
+        else if (this.board[row][col] != ship)
+        {
+            //record the missed attack!
+            this.missedAttacks.push([row,col])
+
+            //we could look to call dom logic here?
+        }
+        {
+
+        }
+    }
 }
 
 //let board = new Gameboard;
